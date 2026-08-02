76faed1b9aeaafd8a49690f9f304d0ca87be71c1
Muhammad Tamir <muhammad.tamir@gmail.com>
2026-08-02 21:59:44 +0700

    chore: fix typo in social card filename

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/scoial-card.webp',
+      image: 'img/social-card.webp',
       colorMode: {
         defaultMode: 'dark',
         disableSwitch: true,
